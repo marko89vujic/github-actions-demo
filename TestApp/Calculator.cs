59ce3455b5e091f05d8d3823b02ff3aecd5d3362
marko vujic <marko.vujic@comparis.ch>
2021-04-04 09:46:09 +0200

    Initial commit

---
+++ TestApp/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            int x = a + b;
+
+            return x;
+        }
+    }
+}
---
+++ UnitTests/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestApp;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void TestAddMethodCalculator()
+        {
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(2, 2);
+
+            Assert.AreEqual(result,4);
+        }
+    }
+}
