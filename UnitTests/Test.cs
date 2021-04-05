49d112f934be90cc936a060ac212e471af4696b0
marko vujic <marko.vujic@comparis.ch>
2021-04-05 08:13:42 +0200

    edit simple-workflow, add Test cases

--- UnitTests/Test.cs
+++ UnitTests/Test.cs
@@ -12,12 +12,15 @@ namespace UnitTests
     class Test
     {
         [Test]
-        public void TestAddMethodCalculator()
+        [TestCase(2, 2, 4)]
+        [TestCase(3, 2, 5)]
+        [TestCase(3, 3, 6)]
+        public void TestAddMethodCalculator(int num1, int num2, int expectedValue)
         {
             Calculator calculator = new Calculator();
-            int result = calculator.Add(2, 2);
+            int result = calculator.Add(num1, num2);
 
-            Assert.AreEqual(result,4);
+            Assert.AreEqual(result,expectedValue);
         }
     }
 }
